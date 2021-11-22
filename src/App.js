74fad36645a4d860aedfd6b99c317629d8db54ac
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2021-11-22 15:01:45 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends Component {
+  state={
+    data:[],
+  }
+  componentDidMount(){
+    axios.get("https://restcountries.com/v3.1/all").then((res)=>{this.setState({data:res.data});
+    console.log(this.state.data);
+  });
+}
+  render() {
+    return (
+      <div className="App">
+       {this.state.data.map((c=> 
+       <div className="countries" key={c.name.common}>
+         <p><img src={c.flags.png} alt="flag" /><h2>{c.name.common}</h2>  </p>
+          <p>Capital is  <strong> {c.capital} </strong></p>
+          <p>Population is <strong> {c.population}</strong></p>
+          
+          </div>
+          ))}
+      </div>
+    );
+  }
 }
 
 export default App;

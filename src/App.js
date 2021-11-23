45a136688f0e99ed48594bc72b4acca2bcd43604
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2021-11-23 13:47:34 +0200

    ready app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import number from 'easy-number-formatter';
 
 class App extends Component {
   state={
     data:[],
+    searchInput: "",
+    isLoading: true,
   }
+
   componentDidMount(){
-    axios.get("https://restcountries.com/v3.1/all").then((res)=>{this.setState({data:res.data});
+    axios.get("https://restcountries.com/v2/all?fields=name,capital,flags,languages,population,currencies").then((res)=>{this.setState({data:res.data,isLoading:false });
     console.log(this.state.data);
   });
 }
+searchHandler=(e)=>{
+  this.setState({
+    searchInput : e.target.value,
+  });
+  console.log(this.state.searchInput)
+}
   render() {
+    if(this.state.isLoading){
+     return( <div>
+        <p className="spin"></p>
+        </div>
+     );
+    }
+    if(!this.state.isLoading){
     return (
+      <>
+      <input type="text" name="search" onChange={this.searchHandler} placeholder="search for country" />
       <div className="App">
-       {this.state.data.map((c=> 
-       <div className="countries" key={c.name.common}>
-         <p><img src={c.flags.png} alt="flag" /><h2>{c.name.common}</h2>  </p>
-          <p>Capital is  <strong> {c.capital} </strong></p>
-          <p>Population is <strong> {c.population}</strong></p>
+       {this.state.data
+       .filter((c) => {
+         return c.name
+         .toLowerCase()
+         .includes(this.state.searchInput.toLowerCase());
+       })
+       .map((c=> 
+       <div className="countries" key={c.name}>
+         <div className="upper"><p><img src={c.flags.png} alt="flag" /><h2>{c.name}</h2>  </p>
+          <p>Capital:  <strong> {c.capital} </strong></p>
+          </div>
+          <p>Language(s):{c.languages.map((lang) =>(
+            <strong> {lang.name} </strong>
+          ))} </p>
+          <p>Currencies:{c.currencies.map((mon,i) =>(
+            <strong key={i}>{mon.name} {mon.symbol} </strong>
+          ))} </p>
+           <p>Population: <strong className="low"> {number.formatNumber(c.population)}</strong></p>
           
           </div>
           ))}
       </div>
+      </>
     );
+  }
   }
 }
 
